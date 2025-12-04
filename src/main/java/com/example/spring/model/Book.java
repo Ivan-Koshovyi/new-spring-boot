31e7815786cbb28e4f26a01ffe70b056abe92668
ivankosh <ivan2023koshovyi@gmail.com>
2025-12-04 15:54:51 +0200

    Add price

--- src/main/java/com/example/spring/model/Book.java
+++ src/main/java/com/example/spring/model/Book.java
@@ -6,12 +6,15 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Data;
 
+import java.math.BigDecimal;
+
 @Entity
 @Data
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Double id;
+    private Long id;
     private String title;
     private Integer size;
+    private BigDecimal price;
 }
