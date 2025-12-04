e065b35d5adeaeed068fcbe10405e8744356f81c
ivankosh <ivan2023koshovyi@gmail.com>
2025-12-04 15:49:49 +0200

    Initial commit

---
+++ src/main/java/com/example/spring/model/Book.java
@@ -0,0 +1,17 @@
+package com.example.spring.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Double id;
+    private String title;
+    private Integer size;
+}
